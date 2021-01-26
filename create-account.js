c065abb9f5bc252d80eb180e47573d69e06e1ba7
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-26 10:22:34 -0700

    account and guessing game

--- create-account.js
+++ create-account.js
@@ -1,5 +1,62 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+  
+    let account = {
+        checkBalance : function(enteredPin){
+                if(amount && enteredPin === pin){
+                    return `$${amount}`;
+                }
+                if (enteredPin !== pin){
+                    return  'Invalid PIN.';
+                }
+                if (enteredPin === pin){
+                    return '$0'
+                }
+        },
+        deposit : function(enteredPin, depositAmount){
+            if (enteredPin !== pin){
+                return  'Invalid PIN.';
+            }
+           
+            if (enteredPin === pin){
+              
+                amount += depositAmount;
+                return `Succesfully deposited $${depositAmount}. Current balance: $${amount}.`
+            }
+        },
+        withdraw : function(enteredPin, withdrawal){
+            if (enteredPin !== pin){
+                return  'Invalid PIN.';
+            }
+            if (withdrawal > amount){
+                return `Withdrawal amount exceeds account balance. Transaction cancelled.`
+            }
+            amount = (amount - withdrawal);
+            return `Succesfully withdrew $${withdrawal}. Current balance: $${amount}.`
+        },
+        changePin : function(oldPin, newPin){
+            if (oldPin !== pin) return "Invalid PIN.";
+            pin = newPin;
+            return "PIN successfully changed!";
+        }
+    }
+return account;
 }
 
 module.exports = { createAccount };
+// Write a function called createAccount which creates a bank account 
+// given a PIN number and an initial deposit amount. The return value should 
+// be an object with four methods on it:
+
+// checkBalance: Given the correct PIN, return the current balance.
+//  (If the PIN is invalid, return “Invalid PIN.”)
+// deposit: Given the correct PIN and a deposit amount,
+//  increment the account balance by the amount. (If the PIN is invalid, 
+//     return “Invalid PIN.”)
+// withdraw: Given the correct PIN and a withdrawal amount, 
+// decrement the account balance by the amount. 
+// You also shouldn’t be able to withdraw more than you have.
+//  (If the PIN is invalid, return “Invalid PIN.”)
+// changePin: Given the old PIN and a new PIN, change
+//  the PIN number to the new PIN.
+//   (If the old PIN is invalid, return “Invalid PIN.”)
+// jest create-account.test.js
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,34 @@
-function guessingGame() {
-
+function guessingGame(num) {
+    let isOver = false;
+let targ = Math.floor(Math.random()* 100);
+let guessCount = 0;
+    function guess(num){
+        if (isOver) return "The game is over, you already won!";
+        guessCount++;
+        if (num === targ){
+            isOver = true;
+            return `You win! You found ${targ} in ${guessCount} guesses.`
+        }
+        if (num < targ){
+            return `${num} is too low!`
+        }
+        if (num > targ){
+            return `${num} is too high!`
+        }
+    }
+return guess;
 }
 
 module.exports = { guessingGame };
+
+// Write a function called guessingGame which returns 
+// a function that allows you to guess a random whole 
+// number between 0 and 99. Every time you create a new game,
+//  it should select a new random number, and keep it secret 
+//  from the player.
+
+// Once the game has started, you can guess the number. 
+// The game should tell you whether your guess is too high,
+//  too low, or correct.
+
+// After a correct guess, the game ends.
